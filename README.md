a2265a660c038822d330bc56b9179d9173b91941
Poarthan <arnold.tianyi.yang@gmail.com>
2024-01-05 19:51:55 -0800

    fixed breaking bug and added backup strategy tests

--- README.md
+++ README.md
@@ -10,8 +10,14 @@ This after 100000 tests, tends to result in around the optimal value 36.6
 Then, we try some variations, like making the distribution normal or specifying to check for who gets on average the best picks.
 
 Most notable is trying to obtain the best possible expected outcome on average. So instead of having the highest chance of getting the best pick exactly, we want the pick's score to be ranked the highest. This increases the damage that not finding the best pick at all in the later 13 does.
-Without changing any other criteria, the stopping and leaping strategy tells us that we should go for around 13 or so, or closer to 1/e<sup>2</sup>.
+Without changing any other criteria, the stopping and leaping strategy tells us that we should go for around 13/14 or so, or closer to 1/e<sup>2</sup>.
 
 More testing is required, and there could be some errors in the way its performed, because everything was thrown together but things are very interesting.
 
 I didn't do much math to confirm any of the values like averaging the top 5, so this is just for fun. Maybe someone has already found this and has some math, I will need to do some more googling.
+
+
+
+After some new tests, for obtaining the highest possible score by trying to give the algorithm a backup option, so after X% of the remianing candidates if it still doesn't find a better score than the best score it's seen, it will now try to find a better score than the second best score it has seen.
+Using this new strategy we improve the score by about 3% from 92 used for stopping and leaping to 95.
+We find that now the optimal stopping point raises to around 19-20 or so and interestingly the X% is once again 1/e or around 36.6%.
--- optimal-stopping-problem-fun.py
+++ optimal-stopping-problem-fun.py
@@ -1,6 +1,7 @@
 import random
 import time
 import numpy as np
+import math
 
 
 def main():
@@ -22,29 +23,52 @@ def main():
     #print("The average of these top 5 stop points is:")
     #print(np.mean([x[0] for x in sorted(analysis,key=lambda x: x[1],reverse=True)[:5]]))
 
-    print("Trying to obtain the highest score on average with just stopping and leaping")
+    #print("Trying to obtain the highest score on average with just stopping and leaping")
+    #tests=10000
+    #analysis=[]
+    #for i in range(99):
+    #    result=avgxtests(100,100,i+1,tests,False)
+    #    print("stop:", i+1, f"average of {tests} tests", result)
+    #    analysis.append([i+1,result])
+
+    #print("The top 5 stop points in order are:")
+    #print(sorted(analysis,key=lambda x: x[1],reverse=True)[:5])
+
+    #print("The average of these top 5 stop points is:")
+    #print(np.mean([x[0] for x in sorted(analysis,key=lambda x: x[1],reverse=True)[:5]]))
+
+    print("Trying to obtain the highest score on average with backup acceptance strategies")
     tests=10000
     analysis=[]
-    for i in range(99):
-        result=avgxtests(100,100,i+1,tests,False)
-        print("stop:", i+1, f"average of {tests} tests", result)
-        analysis.append([i+1,result])
+    for i in range(3,50):
+        for j in range(99):
+            result=leap2tests(100,100,i+1,j,tests,False)
+            print("stop:", i+1, "and acceptance", j, f"average of {tests} tests", result)
+            analysis.append([i+1,j,result])
 
-    print("The top 5 stop points in order are:")
-    print(sorted(analysis,key=lambda x: x[1],reverse=True)[:5])
+    print("The top 25 stop points in order are:")
+    print(sorted(analysis,key=lambda x: x[2],reverse=True)[:25])
 
-    print("The average of these top 5 stop points is:")
-    print(np.mean([x[0] for x in sorted(analysis,key=lambda x: x[1],reverse=True)[:5]]))
+    print("The average stop of these top 25 stop points is:")
+    print(np.mean([x[0] for x in sorted(analysis,key=lambda x: x[2],reverse=True)[:25]]))
+
+    print("The average secondary acceptance of these top 25 stop points is:")
+    print(np.mean([x[1] for x in sorted(analysis,key=lambda x: x[2],reverse=True)[:25]]))
+
 
     #print("norm tests")
-    #tests=1000
+    #tests=10000
     #analysis=[]
     #for i in range(99):
     #    result=normtests(100,100,i+1,tests)
     #    print("stop:", i+1, f"average of {tests} tests", result)
-    #    analysis.append(result)
+    #    analysis.append([i+1,result])
 
-    #print("The best stop point is:", analysis.index(max(analysis))+1)
+    #print("The top 5 stop points in order are:")
+    #print(sorted(analysis,key=lambda x: x[1],reverse=True)[:5])
+
+    #print("The average of these top 5 stop points is:")
+    #print(np.mean([x[0] for x in sorted(analysis,key=lambda x: x[1],reverse=True)[:5]]))
 
 def default_test(options,max_value,stop,infpool=True):
     # options: number of options
@@ -60,6 +84,7 @@ def default_test(options,max_value,stop,infpool=True):
             newOption=random.randint(i,max_value)
             if newOption>best:
                 return i,newOption
+
         return i,newOption
     else:
         assert max_value>=options
@@ -110,6 +135,7 @@ def avgxtests(options,max_value,stop,tests,infpool=True):
                 newOption=random.randint(i,max_value)
                 if newOption>best:
                     allTestScores.append(newOption)
+                    break
             allTestScores.append(newOption)
         return sum(allTestScores)/len(allTestScores)
     else:
@@ -126,8 +152,53 @@ def avgxtests(options,max_value,stop,tests,infpool=True):
                 newOption=randomlist.pop(0)
                 if newOption>best:
                     allTestScores.append(newOption)
+                    break
             allTestScores.append(newOption)
         return sum(allTestScores)/len(allTestScores)
+
+def leap2tests(options,max_value,stop,futurestop,tests,infpool=True):
+    # This is stop and leap but more complicated. The goal here is in case there are no good cases left in the list to slowly start to accept lower and lower values
+    # options: number of options
+    # max_value: maximum value of an option
+    # stop: stop point for observing before leaping
+    # returns: the optimal value
+    nextstop=int(math.floor(futurestop*(options-stop)*0.01))
+    if infpool==True:
+        allTestScores=[]
+        for i in range(tests):
+            collecting = []
+            for i in range(stop):
+                collecting.append(random.randint(0,max_value))
+            best=max(collecting)
+            for i in range(options-stop):
+                newOption=random.randint(i,max_value)
+                if newOption>best:
+                    allTestScores.append(newOption)
+                    break
+            allTestScores.append(newOption)
+        return sum(allTestScores)/len(allTestScores)
+    else:
+        assert max_value>=options
+        allTestScores=[]
+        for i in range(tests):
+            randomlist=[*range(max_value)]
+            random.shuffle(randomlist)
+            collecting = []
+            for i in range(stop):
+                collecting.append(randomlist.pop(0))
+            best=max(collecting)
+            for i in range(options-stop):
+                newOption=randomlist.pop(0)
+                if newOption>best:
+                    allTestScores.append(newOption)
+                    break
+                if i == nextstop:
+                    # now change best to 2nd highest value in collecting
+                    if len(collecting)>1:
+                        best=sorted(collecting)[-2]
+            allTestScores.append(newOption)
+        return sum(allTestScores)/len(allTestScores)
+
 
 def normtests(options,max_value,stop,tests,infpool=True):
     # options: number of options
@@ -136,7 +207,7 @@ def normtests(options,max_value,stop,tests,infpool=True):
     # returns: the optimal value
     allTestScores=[]
     for i in range(tests):
-        genvals=np.random.normal(loc=0, scale=0.2, size=max(max_value,options))
+        genvals=np.random.normal(loc=0, scale=0.5, size=max(max_value,options))
         randomlist=[]
         for i in range(max(options,max_value)):
             randomlist.append(estNumber(float(genvals[i]),max_value))
@@ -148,6 +219,7 @@ def normtests(options,max_value,stop,tests,infpool=True):
             newOption=randomlist.pop(0)
             if newOption>best:
                 allTestScores.append(newOption)
+                break
         allTestScores.append(newOption)
     return sum(allTestScores)/len(allTestScores)
 
